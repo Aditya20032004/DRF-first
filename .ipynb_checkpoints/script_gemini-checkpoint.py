ed250b37ad5c70677e7301844bae854403fb76b2
Aditya20032004 <aditya.23bai10032@gmail.com>
2024-12-26 20:56:32 +0000

    New calculator fir cos and sin

--- .ipynb_checkpoints/script_gemini-checkpoint.py
+++ .ipynb_checkpoints/script_gemini-checkpoint.py
@@ -8,9 +8,15 @@ genai.configure(api_key='AIzaSyA7xPz-EqDFk43IcORE2mat9s1IV0xx-Qw')
 
 model = genai.GenerativeModel('gemini-pro')
 
+
 def generate_commit_message():
-	git_changes = subprocess.run(["git", "diff", "--cached"], capture_output=True, text=True)
-	return model.generate_content('''You are an expert at creating a github commit message for a set of changes. Here is a diff of changes we need a commit message for: '''+ str(git_changes))
+  git_changes = subprocess.run(["git", "diff", "--cached"], capture_output=True, text=True)
+  # Check if there's actual output before generating a message
+  if git_changes.stdout:
+    return model.generate_content('''You are an expert at creating a github commit message for a set of changes. Here is a diff of changes we need a commit message for:''' + str(git_changes))
+  else:
+    # Handle the case where there are no cached changes
+    return "No changes detected. Skipping commit message generation."
 
 if __name__ == '__main__':
-	print(generate_commit_message().text)
+  print(generate_commit_message().text)
